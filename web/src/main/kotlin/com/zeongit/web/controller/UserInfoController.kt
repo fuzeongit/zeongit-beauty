0fa655dee72c424f7282f85f7cd23aa42a067d4f
245142637@qq.com <asd+-*/00>
2020-08-26 11:46:00 +0800

    .

--- web/src/main/kotlin/com/zeongit/web/controller/UserInfoController.kt
+++ web/src/main/kotlin/com/zeongit/web/controller/UserInfoController.kt
@@ -1,14 +1,12 @@
 package com.zeongit.web.controller
 
 
-import com.zeongit.data.constant.BlockState
-import com.zeongit.share.annotations.Auth
 import com.zeongit.share.annotations.CurrentUserInfoId
 import com.zeongit.share.annotations.RestfulPack
 import com.zeongit.share.exception.SignInException
 import com.zeongit.web.core.communal.UserInfoVoAbstract
 import com.zeongit.web.service.*
-import com.zeongit.web.vo.*
+import com.zeongit.web.vo.UserInfoVo
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
@@ -17,10 +15,7 @@ import org.springframework.web.bind.annotation.RestController
 @RestController
 @RequestMapping("userInfo")
 class UserInfoController(override val userInfoService: UserInfoService,
-                         override val followService: FollowService,
-                         private val userBlackHoleService: UserBlackHoleService,
-                         private val pictureBlackHoleService: PictureBlackHoleService,
-                         private val tagBlackHoleService: TagBlackHoleService
+                         override val followService: FollowService
 ) : UserInfoVoAbstract() {
     /**
      * 获取用户信息
